59bec234ccab69cf452f19d913ed097a18ec10d5
Blake <blakewoodruff4@gmail.com>
2020-03-17 18:18:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   string_array=string.chars
-  reverse_string=string_array.each{|b|#{b.reverse}}
+  reverse_string=string_array.each{|b|b.reverse} 
   reverse_string
 end
