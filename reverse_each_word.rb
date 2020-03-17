025408a2ca60a0820c578fa4e9f10ab606203c46
Blake <blakewoodruff4@gmail.com>
2020-03-17 18:27:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(string)
   word_array=string.split
-  reverse_string=(word_array.each{|b| b.reverse}).join(" ") 
+  reverse_string=(word_array.collect{|b| b.reverse}).join(" ") 
 end
